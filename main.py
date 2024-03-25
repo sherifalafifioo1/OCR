61e06219b3dc4507c48c66061c532cad86285538
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-03-25 08:31:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import json
 from flask import Flask, render_template, jsonify, request
+import Authentaction 
+from Authentaction import match_user_id_pic,OCR_pipline 
+import cv2
 
 app = Flask(__name__)
 
 @app.route('/predict_image', methods=['POST'])
 def predict_image():
-    if 'Password' not in request.json:
-        return jsonify({"status": 400, "msg": "Missing 'Password' in request"})
+  # Check if both images are present
+  if 'image1' not in request.files or 'image2' not in request.files:
+    return jsonify({"status": 400, "msg": "Missing one or both images (image1, image2)"}), 400
 
-    password = request.json['Password']
+  # Read images from form data
+    image1 = request.files['image1']
+    image2 = request.files['image2']
     
-    print(password)
+    ID= OCR_pipline(image1)
+    valid=match_user_id_pic(image1,image2)
+    
     
 
-    return jsonify({"id": "123", "valid": "well done"})
+    return jsonify({"Id": Id,"Valid":valid})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, threaded=True)
-
-
-# In[ ]:
-
-
-
-
+     app.run(host='0.0.0.0', port=8000, threaded=True)
