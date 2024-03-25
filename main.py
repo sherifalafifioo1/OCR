897d167745e6b3f9c91796582db470aee6cb887d
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-03-25 20:43:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, render_template, jsonify, request
-import Authentication 
-from Authentication import match_user_id_pic,OCR_pipline 
+from Authentication import match_user_id_pic ,OCR_pipline  
 import cv2
 
 app = Flask(__name__)
