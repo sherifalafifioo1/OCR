4946467cb4620ebf87f90d5025a8c60e4a4460bd
sherifalafifioo1 <147742240+sherifalafifioo1@users.noreply.github.com>
2024-03-26 05:36:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,48 @@
-import json  
-from werkzeug.utils import secure_filename
-from flask import Flask, render_template, jsonify, request
+import json
+import base64
+from flask import Flask, jsonify, request
 from Authentication import match_user_id_pic, OCR_pipline
 import cv2
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/predict_image', methods=['POST'])
 def predict_image():
-  # Check if images are present and valid
-  if 'image1' not in request.files or 'image2' not in request.files:
-    return jsonify({"status": 400, "msg": "Missing one or both images (image1, image2)"}), 400
+    # Check if images are present and valid
+    if 'image1' not in request.json or 'image2' not in request.json:
+        return jsonify({"status": 400, "msg": "Missing one or both images (image1, image2)"}), 400
 
-  try:
-    # Read images with filename sanitization
-    image1 = request.files['image1']
-    image2 = request.files['image2']
-    image1_filename = secure_filename(image1.filename)
-    image2_filename = secure_filename(image2.filename)
+    try:
+        # Retrieve and decode base64-encoded images
+        image1_base64 = request.json['image1']
+        image2_base64 = request.json['image2']
+        image1_data = base64.b64decode(image1_base64)
+        image2_data = base64.b64decode(image2_base64)
 
-    # Process images
-    id_number = OCR_pipline(image1.read())
-    is_valid = match_user_id_pic(image1.read(), image2.read())
+        # Convert image data to numpy arrays
+        image1_array = np.frombuffer(image1_data, np.uint8)
+        image2_array = np.frombuffer(image2_data, np.uint8)
 
-    # Return response with sanitized filenames
-    return jsonify({
-        "status": 200,
-        "data": {
-            "id_number": id_number,
-            "is_valid":str( is_valid)
-        }
-    })
-  except Exception as e:
-    # Handle errors during processing
-    return jsonify({"status": 500, "error": str(e)}), 500
+        # Read images using OpenCV
+        image1 = cv2.imdecode(image1_array, cv2.IMREAD_COLOR)
+        image2 = cv2.imdecode(image2_array, cv2.IMREAD_COLOR)
+
+        # Process images
+        id_number = OCR_pipline(image1)
+        is_valid = match_user_id_pic(image1, image2)
+
+        # Return response
+        return jsonify({
+            "status": 200,
+            "data": {
+                "id_number": id_number,
+                "is_valid": str(is_valid)
+            }
+        })
+    except Exception as e:
+        # Handle errors during processing
+        return jsonify({"status": 500, "error": str(e)}), 500
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000, threaded=True)
+    app.run(host='0.0.0.0', port=8000, threaded=True)
